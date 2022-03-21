30f653bb6cd3f0487cf7cc86c595470aa1125927
Italo Leite <italoleite08@hotmail.com>
2022-03-21 00:59:15 -0300

    circuitBreaker myApp

---
+++ Contador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myApp
+{
+    public class Contador
+    {
+        public int ValorAtual { get; set; }
+    }
+}
---
+++ Post.cs
@@ -0,0 +1,7 @@
+    public class Post
+    {
+        public string userId { get; set; }
+        public string id { get; set; }
+        public string body { get; set; }
+        public string title { get; set; }
+    }
